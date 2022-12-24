6d682b44620535ee782929067a2e5e931f3116ef
Neil Parikh <parikh.neil@me.com>
2022-12-23 23:40:13 -0800

    day4

---
+++ day04.hs
@@ -0,0 +1,38 @@
+main = do
+    rawInput <- getContents
+    let input = parse rawInput
+    print $ solve fullyContained input
+    print $ solve overlap input
+
+solve :: (((Int, Int), (Int, Int)) -> Bool) -> [((Int, Int), (Int, Int))] -> Int
+solve condition = length . filter condition
+
+fullyContained :: ((Int, Int), (Int, Int)) -> Bool
+fullyContained ((a, b), (c, d))
+    | a < c = b >= d
+    | c < a = d >= b
+    | otherwise = True
+
+overlap :: ((Int, Int), (Int, Int)) -> Bool
+overlap ((a, b), (c, d))
+    | a < c = b >= c
+    | c < a = d >= a
+    | otherwise = True
+
+parse :: String -> [((Int, Int), (Int, Int))]
+parse = fmap parseOne . lines
+    where
+    parseOne = listToTuple . fmap (listToTuple . fmap read . wordsOn '-') . wordsOn ','
+
+wordsBy :: (a -> Bool) -> [a] -> [[a]]
+wordsBy f s = case dropWhile f s of
+  [] -> []
+  s' -> w : wordsBy f s''
+    where (w, s'') = break f s'
+
+wordsOn :: (Eq a) => a -> [a] -> [[a]]
+wordsOn x = wordsBy (== x)
+
+listToTuple :: [a] -> (a, a)
+listToTuple [x, y] = (x, y)
+listToTuple _ = error "list does not have exactly 2 elems"
